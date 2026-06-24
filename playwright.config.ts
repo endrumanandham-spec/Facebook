f9572b9483124af67ad36c2f3a98a53f73dc1959
endrumanandham-spec <endrumanandham@.com>
2026-06-24 22:01:40 +0530

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+// Load environment dynamically
+const envName = process.env.TEST_ENV || "dev";
+const envConfig = require(`./config/${envName}.env.ts`).default;
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  timeout: 30 * 1000,          // 30s timeout per test
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL: envConfig.baseURL, // Environment-specific base URL
+    headless: true,             // Run in headless mode
+    screenshot: 'on',           // Capture screenshots
+    video: 'retain-on-failure', // Record video only on failures
+  },
+reporter: [
+    ['list'],                   // Console output
+    ['html', { outputFolder: 'playwright-report' }] // HTML report
+  ],
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } }
+  ]
+});
---
+++ tests/fbLogin.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+const envConfig = require(`../config/${process.env.TEST_ENV || "dev"}.env.ts`).default;
+
+test('login works in selected environment', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await page.goto(envConfig.baseURL);
+  //await loginPage.gotoLogin();
+  
+
+  await loginPage.login(envConfig.username, envConfig.password);
+  //await expect(loginPage.welcomeMessage).toHaveText(/Welcome/);
+}); 
+
+
+
+test('Create Account', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.getByRole('link', { name: 'Create new account' }).click();
+  await page.getByRole('textbox', { name: 'Name First name Surname' }).fill('John');
+  await page.getByRole('textbox', { name: 'Surname', exact: true }).fill('Cena');
+
+  
+  
+  await page.getByLabel('Select day').locator('div').filter({ hasText: /^Day$/ }).click();
+  await page.locator('div').filter({ hasText: /^3$/ }).nth(1).click();
+  await page.getByLabel('Select month').locator('div').filter({ hasText: /^Month$/ }).click();
+  await page.getByText('June').click();
+  await page.getByLabel('Select year').locator('div').filter({ hasText: /^Year$/ }).click();
+  await page.getByText('2021', { exact: true }).click();
+  await page.locator('div').filter({ hasText: /^Select your gender$/ }).nth(1).click();
+  await page.getByText('Male', { exact: true }).click();
+  await page.getByRole('textbox', { name: 'Mobile number or email' }).click();
+  await page.getByRole('textbox', { name: 'Mobile number or email' }).fill('sdfsdf');
+  await page.getByRole('textbox', { name: 'Password Password' }).click();
+  await page.getByRole('textbox', { name: 'Password Password' }).fill('sdfsdf');
+  await page.getByRole('button', { name: 'Submit' }).click();
+});
